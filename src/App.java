29c63b3f8941b4b039c42165c835baa047a4484d
MMartincorena <martinmartincorena@gmail.com>
2022-08-11 02:34:39 -0300

    Inicio

---
+++ src/App.java
@@ -0,0 +1,37 @@
+/*Primera parte:
+Crear una función con tres parámetros que sean números que se suman entre sí.
+Llamar a la función en el main y darle valores.
+-------------------------------------------------------------------------------------------------------
+Segunda parte:
+Crear una clase coche.
+Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+Una función que incremente el número de puertas que tiene el coche.
+Crear un objeto miCoche en el main y añadirle una puerta.
+Mostrar el número de puertas que tiene el objeto.
+*/
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    //int resultado = suma(10, 20, 15);
+    //System.out.println(resultado);
+    Coche miCoche = new Coche();
+    miCoche.SumaPuerta();
+    miCoche.SumaPuerta();
+    miCoche.SumaPuerta();
+
+    System.out.println(miCoche.numeroPuertas);
+  }
+  /*  public static int suma(int num1, int num2, int num3) {
+    return num1 + num2 + num3;
+  }*/
+}
+
+class Coche {
+
+  public int numeroPuertas = 0;
+
+  public void SumaPuerta() {
+    this.numeroPuertas++;
+  }
+}
